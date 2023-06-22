2368c5659abeb6aae2e1886be7fd111e3602d558
andrewnnov <andrewnnov@yandex.ru>
2023-06-22 14:19:36 -0300

    task4

--- tasks4.js
+++ tasks4.js
@@ -54,3 +54,84 @@ arrOfSt.sort(function (a, b) {
 console.log(arrOfSt);
 
 console.log("a" < "b");
+
+let str2 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let arr2 = [];
+
+for (let j = 0; j < str2.length; j++) {
+  let charCode = str2.charCodeAt(j);
+  arr2.push(charCode);
+}
+
+console.log(arr2);
+
+let total1 = arr2.join("");
+console.log(total1);
+
+let total2 = arr2.join("").replaceAll("7", "1");
+console.log(total2);
+
+let sum1 = 0;
+
+for (let elem of total1) {
+  sum1 += parseInt(elem);
+}
+
+let sum2 = 0;
+
+for (let elem of total2) {
+  sum2 += parseInt(elem);
+}
+
+let total = sum1 - sum2;
+
+console.log(total);
+
+let str3 = "wc";
+
+let arr3 = [
+  "25",
+  "7",
+  "8",
+  "4",
+  "14",
+  "23",
+  "8",
+  "25",
+  "23",
+  "29",
+  "16",
+  "16",
+  "4",
+];
+
+let alf = [];
+
+for (let j = 122; j > 96; j--) {
+  let s = String.fromCharCode(j);
+  alf.push(s);
+}
+
+alf.push("!");
+alf.push("?");
+alf.push(" ");
+
+let arrWithLetter = [];
+
+for (let j = 0; j < arr3.length; j++) {
+  let elem = parseInt(arr3[j]) - 1;
+  arrWithLetter.push(alf[elem]);
+}
+
+console.log(alf);
+
+console.log(arrWithLetter.join(""));
+
+let x = "'man i need a taxi up to ubud'";
+
+let arrSr = x
+  .split(" ")
+  .sort((a, b) => a.charCodeAt(a.length - 1) - b.charCodeAt(b.length - 1));
+
+console.log(arrSr);
